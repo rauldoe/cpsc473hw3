7521ca324b3ceffbba7b9aeefecf8fa811df26e9
Raul Do <rauldo@Rauls-MacBook-Pro.local>
2017-09-25 19:39:40 -0700

    added nodejs tests

--- coffeerun/tests/execfile.js
+++ coffeerun/tests/execfile.js
@@ -1,7 +1,7 @@
 var vm = require("vm");
 var fs = require("fs");
 
-exports.load = function(path, context) {
+module.exports = function(path, context) {
   context = context || {};
   var data = fs.readFileSync(path);
   vm.runInNewContext(data, context, path);
--- coffeerun/tests/main.js
+++ coffeerun/tests/main.js
@@ -1,2 +1,23 @@
+var sourceDataStore = "../scripts/datastore.js";
+var sourceTruck = "../scripts/truck.js";
+var sourceTestDataStore = "./test_datastore.js";
+var sourceTestTruck = "./test_truck.js";
 
-var window = window || {};
+var window = {};
+var fs = require("fs");
+
+// Read and eval library
+var fileData = {};
+
+fileData = fs.readFileSync(sourceDataStore, 'utf8');
+eval(fileData);
+fileData = fs.readFileSync(sourceTestDataStore, 'utf8');
+eval(fileData);
+fileData = fs.readFileSync(sourceTruck, 'utf8');
+eval(fileData);
+fileData = fs.readFileSync(sourceTestTruck, 'utf8');
+eval(fileData);
+
+var truckId = "test_id-nodejs";
+var ds = test_obj_datastore(window);
+test_obj_truck(window, truckId, ds);
